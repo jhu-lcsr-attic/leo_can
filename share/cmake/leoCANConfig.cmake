03fd40cf3d50c4813de221a6c97435c8761d9839
Jonathan Bohren <jonathan.bohren+hg@gmail.com>
2012-02-20 00:57:53 -0500

    making cmakelists robust to missing ros, adding leoCANConfig

---
+++ share/cmake/leoCANConfig.cmake
@@ -0,0 +1,5 @@
+
+set( leoCAN_INCLUDE_DIRS "/home/jbohren/versioned/leoCAN/include;/usr/xenomai/include" )
+set( leoCAN_LIBRARY_DIRS "/usr/xenomai/lib;/home/jbohren/versioned/leoCAN/lib" )
+set( leoCAN_LIBRARIES   "/usr/xenomai/lib/librtdm.so;/usr/xenomai/lib/libxenomai.so;leoCAN" )
+set( leoCAN_DEFINITIONS "-D_GNU_SOURCE -D_REENTRANT -D__XENO__" )
